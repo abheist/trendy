a06bbc2ba490a7e28b368ee4015f350a0cba49c1
Abhishek <abhishek291093@gmail.com>
2016-02-04 20:31:56 +0530

    Completed the sycing and add some icons and all

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 var app = angular.module('trendy', []);
-
-app.controller('MainCtrl', ['$http', function($http){
-	
+app.controller('MainCtrl', ['$http', '$scope', function($http, $scope) {
+    $http.get('data/data.json').success(function(data) {
+        $scope.details = data;
+    });
 }]);
